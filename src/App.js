87523b0c828b9e1db28f944aa02ac0dd278d6c16
vannara43 <36809428+vannara43@users.noreply.github.com>
2022-05-17 04:31:20 -0700

    added experience

--- src/App.js
+++ src/App.js
@@ -29,8 +29,8 @@ function App() {
       </div>
 
       <section className='section shadow'>
-        <h1 className='display-5 ' style={{ letterSpacing: "1px" }}>Welcome to my portfolio</h1>
-        <p>I love to create and problem solve! I'm a magical being that is sometime refered as a "full-stack developer", my primary work is front-end. My abilities are to create nothing into existence.</p>
+        <h1 className='display-5 ' style={{ letterSpacing: "1px" }}>"It's only going to get bigger"</h1>
+        <p>Welcome to my portfolio! I love to create and problem solve. I'm a magical being that is sometime refered as a "full-stack developer", my primary work is front-end. My abilities are to create nothing into existence.</p>
         <p>Check out my projects below.</p>
       </section>
 
@@ -70,10 +70,38 @@ function App() {
           </Routes>
         </Router>
       </div>
+
+      <section className='section shadow'>
+        <h1 className='display-5 ' style={{ letterSpacing: "1px" }}>Experience</h1> <h3>Interrogas | Full-time | Oct 2021 - May (Current) | 8 mos</h3>
+        <div className="row">
+          <div className="col">
+            ● I have utilized SQL, ADO.NET, and React for this project.
+            ● Created a database, use of framework, and front-end design to accomplish this work.
+            ● Created data tables and stored procedures with SQL Server Management
+            Studio(SSMS).
+            ● Designed and managed relational databases, stored procedures, foreign key
+            constraints, sub-queries and table joins with SQL Server Management Studio(SSMS).
+            ● Designed and Implemented RESTful API's using .NET Core, ADO.NET, in Visual Studio
+            2022
+            ● Utilize UI to makes calls to the built API's to allow users to create and manage data
+            ● Utilize Bootstrap along with React JS that allows users to complete content management
+            procedures.
+            ● Built a responsive application using React.js
+            ● Conducted code reviews and merged pull requests using Git and GitHub. Provided
+            constructive and meaningful feedback, focused on strengthening both individual and
+            AGILE development team product knowledge
+          </div>
+        </div>
+      </section>
+
+      <Cardv1 />
+
       <div className="text-center p-3">
         <a href="/myprojects/#card" className='btn btn-outline-dark border border-dark' style={{ boxShadow: "0px 0px 2px 0px #001cff" }}>Return to top</a><br />
       </div>
-    </React.Fragment>
+
+
+    </React.Fragment >
   );
 }
 
